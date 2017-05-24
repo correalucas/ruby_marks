93061bd3327fb33a134613c2fbbac05355c3333f
Lucas Correa <lucascorrea@claretiano.edu.br>
2017-05-24 18:03:06 -0300

    Bug fix of grid scan

--- lib/ruby_marks/flood_scan.rb
+++ lib/ruby_marks/flood_scan.rb
@@ -3,10 +3,11 @@ module RubyMarks
   class FloodScan
 
     def scan(image, node, width, height)
-      target = Magick::Pixel.new(65535, 65535, 65535, 0) 
+      original_node = node
+      target = Magick::Pixel.new(65535, 65535, 65535, 0)
       replacement = Magick::Pixel.new(0, 0, 0, 0)
       queue = Array.new
-      queue.push(node) 
+      queue.push(node)
       vx = Hash.new { |hash, key| hash[key] = [] }
       vy = Hash.new { |hash, key| hash[key] = [] }
       steps = 0
@@ -20,7 +21,7 @@ module RubyMarks
           x -= 1
         end
         while x < image.columns && image.get_pixels(x, y, 1, 1)[0] == target
-          image.store_pixels(x, y, 1, 1, [replacement]) 
+          image.store_pixels(x, y, 1, 1, [replacement])
           if !span_up && y > 0 && image.get_pixels(x, y - 1, 1, 1)[0] == target
             queue.push(Magick::Point.new(x, y - 1))
             span_up = true
@@ -33,19 +34,21 @@ module RubyMarks
           elsif span_down && y < image.rows - 1 && image.get_pixels(x, y + 1, 1, 1)[0] != target
             span_down = false
           end
-          vx[y] << x 
+          vx[y] << x
           vy[x] << y
           x += 1
           steps += 1
         end
-        queue.push(Magick::Point.new(x, y - 1)) if queue.empty? && steps < 100 
+        queue.push(Magick::Point.new(x, y - 1)) if queue.empty? && steps < 100
       end
       vx = vx.find_mesure(width, 5).max_frequency
       vy = vy.find_mesure(height, 5).max_frequency
       if vx && vy
         {x1: vx[0][0], y1: vy[0][0], x2: vx[0][1], y2: vy[0][1]}
       else
-        {}
+        original_node.x += 5
+        original_node.y += 5
+        scan(image, original_node, width, height)
       end
     end
   end
